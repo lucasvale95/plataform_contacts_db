05ef230eb97ad9a23e1d4fdcd112cc7760420177
Lucas <lucas.sv95@hotmail.com>
2023-02-06 11:48:10 -0300

    feat: config deploy

--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,5 @@ import AppDataSource from "./data-source";
         console.error("Error during Data Source initialization", err)
     })
     
-    app.listen(process.env.PORT)
+    return app.listen(process.env.PORT || 3000)
 })()
