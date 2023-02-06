e132245b1f0826030d7a254e7fdcfe6772683f60
Lucas <lucas.sv95@hotmail.com>
2023-02-06 11:14:02 -0300

    feat: config deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,26 +2,12 @@ import { DataSource } from "typeorm"
 import path from "path"
 import "dotenv/config"
 
-const AppDataSource = new DataSource(
-    process.env.NODE_ENV === "test" ?
-    {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
-        type: "postgres",
-        host: "localhost",
-        port: parseInt(process.env.PGPORT!),
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        logging: true,
-        synchronize: false,
-        entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-        migrations: [path.join(__dirname, "./migrations/**.{js,ts}")]
-    }
+const AppDataSource = new DataSource({
+	type: 'postgres',
+	url: process.env.DB_URI,
+	entities: [`${__dirname}/**/entities/*.{ts,js}`],
+	migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+}
 )
 
 export default AppDataSource
